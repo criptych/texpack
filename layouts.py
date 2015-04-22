fbbcd396056085d777fcb56d82148be6e2ecb907
criptych <criptych@gmail.com>
2015-04-22 16:03:25 -0400

    Initial commit

---
+++ layouts.py
@@ -0,0 +1,351 @@
+################################################################################
+## TexPack layout engine
+################################################################################
+
+__all__ = ['get_layout']
+
+################################################################################
+
+def get_next_power_of_2(n):
+    n = int(n) & 0x7fffffffffffffff
+    n -= 1
+    n |= n >> 32
+    n |= n >> 16
+    n |= n >>  8
+    n |= n >>  4
+    n |= n >>  2
+    n |= n >>  1
+    n += 1
+    return n
+
+################################################################################
+
+class Rect(object):
+    def __init__(self, w=0, h=0, x=0, y=0):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def __repr__(self):
+        return 'Rect<x=%s,y=%s,w=%s,h=%s>' % (self.x, self.y, self.w, self.h)
+
+    @property
+    def left(self):
+        return self.x
+    @left.setter
+    def left(self, value):
+        self.x = value
+
+    @property
+    def top(self):
+        return self.y
+    @top.setter
+    def top(self, value):
+        self.y = value
+
+    @property
+    def right(self):
+        return self.x + self.w
+    @right.setter
+    def right(self, value):
+        self.w = max(0, value - self.x)
+
+    @property
+    def bottom(self):
+        return self.y + self.h
+    @bottom.setter
+    def bottom(self, value):
+        self.h = max(0, value - self.y)
+
+    @property
+    def width(self):
+        return self.w
+    @width.setter
+    def width(self, value):
+        self.w = max(0, value)
+
+    @property
+    def height(self):
+        return self.h
+    @height.setter
+    def height(self, value):
+        self.h = max(0, value)
+
+    @property
+    def empty(self):
+        return self.w <= 0 and self.h <= 0
+
+    def intersects(self, rect):
+        return self.x + self.w > rect.x and self.x < rect.x + rect.w and \
+               self.y + self.h > rect.y and self.y < rect.y + rect.h
+
+    def intersect(self, rect):
+        r = Rect()
+        r.left   = max(self.left,   rect.left  )
+        r.top    = max(self.top,    rect.top   )
+        r.right  = min(self.right,  rect.right )
+        r.bottom = min(self.bottom, rect.bottom)
+        return r
+
+    def union(self, rect):
+        return
+
+################################################################################
+
+from PIL import Image
+import os
+
+class Sprite(object):
+    def __init__(self, filename, *args, **kwargs):
+        self.image = Image.open(filename).convert('RGBA')
+        self.pixels = self.image.load()
+        self.name = os.path.basename(filename)
+        self.rotated = False
+
+    def rotate(self):
+        if self.rotated:
+            self.image = self.image.transpose(Image.ROTATE_270)
+        else:
+            self.image = self.image.transpose(Image.ROTATE_90)
+        self.rect.width, self.rect.height = self.rect.height, self.rect.width
+        self.rotated = not self.rotated
+
+    @property
+    def rect(self):
+        rect = vars(self).get('_rect')
+        if rect is None:
+            rect = self._rect = Rect(*self.image.size)
+        return rect
+
+class Sheet(object):
+    def __init__(
+        self,
+        layout=None,
+        min_size=None,
+        max_size=None,
+        rotate=False,
+        npot=False,
+        square=False,
+    ):
+        try:
+            min_size = int(min_size)
+            min_size = min_size, min_size
+        finally:
+            pass
+
+        try:
+            max_size = int(max_size)
+            max_size = max_size, max_size
+        finally:
+            pass
+
+        if not npot:
+            min_size = (
+                get_next_power_of_2(max_size[0]),
+                get_next_power_of_2(max_size[1])
+                )
+            max_size = (
+                get_next_power_of_2(max_size[0]),
+                get_next_power_of_2(max_size[1])
+                )
+
+        self.layout = layout(self)
+        self.min_size = min_size
+        self.max_size = max_size
+        self.rotate = rotate
+        self.npot = npot
+        self.square = square
+
+        self.clear()
+
+    def clear(self):
+        self.sprites = []
+        self.size = self.min_size
+
+    def grow(self):
+        maxw, maxh = self.max_size
+        w, h = self.size
+        w = max(1, w * 2)
+        h = max(1, h * 2)
+        if maxw > 0: w = min(w, maxw)
+        if maxh > 0: h = min(h, maxh)
+        if not self.npot:
+            w = get_next_power_of_2(w)
+            h = get_next_power_of_2(h)
+        self.size = w, h
+
+    def check(self, rect):
+        w, h = self.size
+        rw, rh = rect.x + rect.w, rect.y + rect.h
+        return rw > w or rh > h
+
+    def add(self, sprites):
+        remain = []
+
+        if self.layout:
+            for s in sprites:
+                if self.layout.add(s):
+                    self.sprites.append(s)
+                    while self.check(s.rect):
+                        self.grow()
+                else:
+                    remain.append(s)
+            return remain
+        else:
+            return sprites
+
+################################################################################
+
+class Layout(object):
+    """
+    Base class for rectangle layout algorithms.
+    """
+
+    def __init__(self, sheet):
+        self.sheet = sheet
+
+    def add(self, *sprites):
+        raise NotImplementedError('use a subclass of Layout')
+
+################################################################################
+
+class ShelfLayout(Layout):
+    """
+    A basic layout that arranges rects in order on progressively higher rows or
+    "shelves".  When each rect is placed, if it does not fit on the current
+    shelf, a new shelf is created at the top of the tallest item.
+    """
+
+    class Shelf(object):
+        def __init__(self, top, height):
+            self.top = top
+            self.height = height
+            self.width = 0
+            self.rects = []
+
+        def place(self, rect):
+            self.rects.append(rect)
+            rect.left = self.width
+            rect.top = self.top
+            self.width += rect.width
+            if self.height < rect.height:
+                self.height = rect.height
+            return rect
+
+    def __init__(self, *args, **kwargs):
+        Layout.__init__(self, *args, **kwargs)
+        self.shelves = []
+
+    def add(self, spr):
+        rect = spr.rect
+
+        shelf = vars(self).setdefault('shelf')
+
+        if shelf is None:
+            shelf = ShelfLayout.Shelf(0, 0)
+            self.shelves.append(shelf)
+
+        if self.sheet.rotate and rect.width > rect.height and rect.width <= shelf.height:
+            spr.rotate()
+
+        if self.sheet.max_size[0] > 0 and shelf.width + rect.width > self.sheet.max_size[0]:
+            shelf = ShelfLayout.Shelf(shelf.top + shelf.height, 0)
+            self.shelves.append(shelf)
+
+        self.shelf = shelf
+
+        if self.sheet.max_size[1] > 0 and shelf.top + rect.height > self.sheet.max_size[1]:
+            return False
+
+        shelf.place(rect)
+        return True
+
+################################################################################
+
+class StackLayout(Layout):
+    """
+    Like ShelfLayout, but arranges rects in columns.
+    """
+
+    class Stack(object):
+        def __init__(self, left, width):
+            self.left = left
+            self.width = width
+            self.height = 0
+            self.rects = []
+
+        def place(self, rect):
+            self.rects.append(rect)
+            rect.left = self.left
+            rect.top = self.height
+            self.height += rect.height
+            if self.width < rect.width:
+                self.width = rect.width
+            return rect
+
+    def __init__(self, *args, **kwargs):
+        Layout.__init__(self, *args, **kwargs)
+        self.stacks = []
+
+    def add(self, spr):
+        rect = spr.rect
+
+        stack = vars(self).setdefault('stack')
+
+        if stack is None:
+            stack = StackLayout.Stack(0, 0)
+            self.stacks.append(stack)
+
+        if self.sheet.rotate and rect.height > rect.width and rect.height <= stack.width:
+            spr.rotate()
+
+        if self.sheet.max_size[1] > 0 and stack.height + rect.height > self.sheet.max_size[1]:
+            stack = StackLayout.Stack(stack.left + stack.width, 0)
+            self.stacks.append(stack)
+
+        self.stack = stack
+
+        if self.sheet.max_size[0] > 0 and stack.left + rect.width > self.sheet.max_size[0]:
+            return False
+
+        stack.place(rect)
+        return True
+
+################################################################################
+
+class MaxRectsLayout(Layout):
+    """
+    A layout that arranges rects by subdividing free space into overlapping
+    regions.  When each rect is placed, its area is removed from the remaining
+    free-space rects.
+    """
+
+    def add(self, *rects):
+        raise NotImplementedError('MaxRectsLayout is not implemented')
+
+################################################################################
+
+class SkylineLayout(Layout):
+    """
+    """
+
+    def add(self, *rects):
+        raise NotImplementedError('SkylineLayout is not implemented')
+
+################################################################################
+
+LAYOUTS = {
+    'shelf': ShelfLayout,
+    'stack': StackLayout,
+    'maxrects': MaxRectsLayout,
+    'skyline': SkylineLayout,
+}
+
+def get_layout(name):
+    return LAYOUTS.get(name)
+
+################################################################################
+## EOF
+################################################################################
+
---
+++ texpack.py
@@ -0,0 +1,329 @@
+#!/bin/env python
+################################################################################
+## TexPack - Free and open-source TexturePacker alternative
+## Copyright (c) 2015 criptych
+##
+## Released under the MIT License.  See LICENSE file for terms.
+##
+## Disclaimer:  This tool was not developed from TexturePacker source or by
+## reverse engineering of any kind; but through inspection of the TexturePacker
+## documentation and independent research and implementation of the features
+## advertised therein.
+##
+################################################################################
+
+import logging
+log = logging.getLogger('TexPack')
+
+import math
+
+from PIL import Image
+from PIL import ImageChops
+from PIL import ImageOps
+
+import layouts
+
+################################################################################
+
+def load_sprites(filenames):
+    from glob import glob
+    return [layouts.Sprite(f) for fn in filenames for f in glob(fn)]
+
+def mask_sprites(sprites):
+    for i, spr in enumerate(sprites):
+        w, h = spr.image.size
+
+        A = spr.image.getpixel((0,  0  ))
+        B = spr.image.getpixel((w-1,0  ))
+        C = spr.image.getpixel((0,  h-1))
+        D = spr.image.getpixel((w-1,h-1))
+
+        ## TODO: there must be a less ugly way to do this
+        AB = A == B
+        AC = A == C
+        AD = A == D
+        BC = B == C
+        BD = B == D
+        CD = C == D
+
+        if AB and (BC or BD):
+            bg = A
+        elif (AC or BC) and CD:
+            bg = D
+        else:
+            ## no matches, skip it
+            continue
+
+        ## based on:
+        ## <https://mail.python.org/pipermail/image-sig/2002-December/002092.html>
+
+        outbands = [srcband.point(lambda p: (p != level) and 255)
+                    for srcband, level in zip(spr.image.split(), bg)]
+        tband = ImageChops.lighter(
+            ImageChops.lighter(outbands[0], outbands[1]),
+            outbands[2]).convert('1')
+        spr.image.putalpha(tband)
+
+    return sprites
+
+def trim_sprites(sprites):
+    for i, spr in enumerate(sprites):
+        box = spr.image.getbbox()
+        spr.image = spr.image.crop(box)
+    return sprites
+
+def hash_sprites(sprites):
+    return sprites
+
+def alias_sprites(sprites):
+    return sprites
+
+def extrude_sprites(sprites, size):
+    if size:
+        for i, spr in enumerate(sprites):
+            w, h = spr.image.size
+            image = Image.new(spr.image.mode, (w+size*2, h+size*2), (0,0,0,0))
+            A = spr.image.crop((0,  0,  1,1)).resize((size,size))
+            B = spr.image.crop((0,  0,  w,1)).resize((w,   size))
+            C = spr.image.crop((w-1,0,  w,1)).resize((size,size))
+            D = spr.image.crop((0,  0,  1,h)).resize((size,h   ))
+            E = spr.image.crop((w-1,0,  w,h)).resize((size,h   ))
+            F = spr.image.crop((0,  h-1,1,h)).resize((size,size))
+            G = spr.image.crop((0,  h-1,w,h)).resize((w,   size))
+            H = spr.image.crop((w-1,h-1,w,h)).resize((size,size))
+            image.paste(A, (0,     0     ), A)
+            image.paste(B, (size,  0     ), B)
+            image.paste(C, (size+w,0     ), C)
+            image.paste(D, (0,     size  ), D)
+            image.paste(E, (size+w,size  ), E)
+            image.paste(F, (0,     size+h), F)
+            image.paste(G, (size,  size+h), G)
+            image.paste(H, (size+w,size+h), H)
+            image.paste(spr.image, (size,size), spr.image)
+            spr.image = image
+    return sprites
+
+def pad_sprites(sprites, size):
+    if size:
+        for i, spr in enumerate(sprites):
+            w, h = spr.image.size
+            image = Image.new(spr.image.mode, (w+size, h+size), (0,0,0,0))
+            image.paste(spr.image, (0, 0), spr.image)
+            spr.image = image
+    return sprites
+
+def quantize_texture(texture, quantize, palette_type, palette_depth, dither):
+    colors = 2**int(palette_depth)
+
+    if quantize == 'median-cut':
+        method = 0
+    elif quantize == 'octree':
+        method = 2
+    else:
+        ## unsupported method
+        return texture
+
+    if palette_type == 'web-safe':
+        palette = []
+    else:
+        palette = []
+
+    return texture.quantize(colors=colors, method=method, palette=palette)
+
+################################################################################
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(usage='%(prog)s prefix sprites... [options]')
+
+    parser.add_argument('prefix',
+                        help="Prefix for output sheet textures")
+    parser.add_argument('sprites', nargs='+',
+                        help="Sprite images / folders / wildcards")
+
+    ########################################################################
+
+    sprite_group = parser.add_argument_group('sprite options')
+    sprite_group.add_argument('--trim', action='store_true', default=False,
+                              help="Trim sprites to visible area.")
+    sprite_group.add_argument('--pad', type=int, default=0, nargs='?', const=1, metavar='SIZE',
+                              help="Insert %(metavar)s pixels of padding between sprites. "
+                              "If %(metavar)s is omitted, defaults to `%(const)s'.")
+    sprite_group.add_argument('--extrude', type=int, default=0, nargs='?', const=1, metavar='SIZE',
+                              help="Extrude sprite edges %(metavar)s pixels to avoid color bleed. "
+                              "If %(metavar)s is omitted, defaults to `%(const)s'.")
+    sprite_group.add_argument('--alias', action='store_true', default=False,
+                              help="Find and remove duplicate sprites.")
+    sprite_group.add_argument('--mask', action='store_true', default=False,
+                              help="Mask sprites against background color.")
+
+    ########################################################################
+
+    layout_group = parser.add_argument_group('layout options')
+    layout_group.add_argument('--layout', type=str.lower, default='shelf', metavar='TYPE',
+                              choices=['shelf','stack','max-rects','skyline'],
+                              help="Select layout algorithm. (default: %(default)s)")
+    layout_group.add_argument('--rotate', action='store_true', default=False,
+                              help="Allow layout engine to rotate sprites.")
+    layout_group.add_argument('--npot', action='store_true', default=False,
+                              help="Allow non-power-of-two sheet dimensions.")
+    layout_group.add_argument('--square', action='store_true', default=False,
+                              help="Constrain sheet dimensions to a square.")
+    layout_group.add_argument('--min-size', type=int, default=0, metavar='SIZE',
+                              help="Set minimum sheet dimensions.")
+    layout_group.add_argument('--max-size', type=int, default=0, metavar='SIZE',
+                              help="Set maximum sheet dimensions.")
+
+    ########################################################################
+
+    texture_group = parser.add_argument_group('texture options')
+    texture_group.add_argument('--scale', action='store_true', default=False,
+                               help="Produce full- and half-scale images.")
+    texture_group.add_argument('--color-depth', type=str.lower, default='RGBA8', metavar='DEPTH',
+                               choices=['RGB4','RGBA4','RGB5','RGB565','RGBA5551','RGB8','RGBA8'],
+                               help="Select color bit-depth. (default: %(default)s)")
+    texture_group.add_argument('--compress', type=str.lower, nargs='?', const='S3TC', metavar='TYPE',
+                               choices=['S3TC','ETC','PVRTC','ATITC'], help=
+                               "Set texture compression. If %(metavar)s is omitted, defaults to `%(const)s'. "
+                               "Overrides --depth, --format, and quantization options.")
+    texture_group.add_argument('--quantize', type=str.lower, nargs='?', const='median-cut', metavar='TYPE',
+                               choices=['median-cut','histogram','octree','k-means','kohonen','spatial'],
+                               help="Select quantization method for indexed textures. "
+                               "If %(metavar)s is omitted, defaults to `%(const)s'. Overrides --depth.")
+    texture_group.add_argument('--palette-type', type=str.lower, default='adaptive', metavar='TYPE',
+                               choices=['web-safe','adaptive'],
+                               help="Select palette type. (default: %(default)s)")
+    texture_group.add_argument('--palette-depth', type=int, default=8, choices=[1,2,4,8], metavar='DEPTH',
+                               help="Select palette bit-depth. (default: %(default)s)")
+    texture_group.add_argument('--dither', type=str.lower, nargs='?', const='void-cluster', metavar='TYPE',
+                               choices=['random','halftone','bayer','void-cluster','diffusion'],
+                               help="Select dithering method for indexed textures. "
+                               "If %(metavar)s is omitted, defaults to `%(const)s'.")
+
+    ########################################################################
+
+    data_group = parser.add_argument_group('data options')
+    data_group.add_argument('--format', type=str.lower, default='png',
+                            help="Select default output texture format.")
+    data_group.add_argument('--index', default='default',
+                            help="Select output sprite index format.")
+    data_group.add_argument('--encrypt', type=str.lower, metavar='TYPE',
+                            choices=['xor','aes-ecb','aes-cbc'],
+                            help="Select algorithm used to encrypt output textures.")
+    data_group.add_argument('--key', metavar='KEY',
+                            help="Provide encryption key directly.")
+    data_group.add_argument('--key-hash', type=str.lower, metavar='HASH',
+                            choices=['MD5','SHA-1','SHA-128','SHA-256','SHA-512','SHA-1024'],
+                            help="Select hash algorithm for value of --key.")
+    data_group.add_argument('--key-file', metavar='FILE',
+                            help="Provide encryption key from %(metavar)s. Overrides --key.")
+
+    ########################################################################
+
+    args = parser.parse_args()
+
+    ########################################################################
+    ## Phase 1 - Load and process individual sprites
+
+    sprites = load_sprites(args.sprites)
+
+    if not sprites:
+        parser.error('No sprites found.')
+
+    if args.mask:
+        ## Mask sprites against background color
+        sprites = mask_sprites(sprites)
+
+    if args.trim:
+        ## Trim sprites to visible area
+        sprites = trim_sprites(sprites)
+    else:
+        ## Generate hashes of trimmed sprites
+        sprites = hash_sprites(sprites)
+
+    if args.alias:
+        ## Find and remove duplicate sprites
+        sprites = alias_sprites(sprites)
+
+    if args.extrude:
+        ## Extrude sprite edges to avoid color bleed
+        sprites = extrude_sprites(sprites, args.extrude)
+
+    if args.pad:
+        ## Insert transparent padding between sprites
+        sprites = pad_sprites(sprites, args.pad)
+
+    sprites.sort(key=lambda s: s.rect.w * s.rect.h, reverse=True)
+
+    ########################################################################
+    ## Phase 2 - Arrange sprites in sheets
+
+    sheets = []
+
+    layout = layouts.get_layout(args.layout)
+
+    while sprites:
+        sheet = layouts.Sheet(
+            min_size = args.min_size,
+            max_size = args.max_size,
+            rotate = args.rotate,
+            npot = args.npot,
+            square = args.square,
+            layout = layout
+        )
+
+        sheets.append(sheet)
+
+        sprites = sheet.add(sprites)
+
+    ########################################################################
+    ## Phase 3 - Scale, quantize, and compress textures
+
+    if args.scale:
+        pass
+
+    if args.compress:
+        pass
+
+    numsheets = len(sheets)
+
+    if numsheets > 0:
+        digits = int(math.floor(math.log10(numsheets))+1)
+    else:
+        digits = 0
+
+    print numsheets, 'sheet(s):'
+
+    for i, sheet in enumerate(sheets):
+        texture = Image.new('RGBA', sheet.size) # args.color_depth
+
+        for sprite in sheet.sprites:
+            texture.paste(sprite.image, (sprite.rect.x, sprite.rect.y), sprite.image)
+
+        texture = quantize_texture(texture, args.quantize, args.palette_type, args.palette_depth, args.dither)
+
+    ########################################################################
+    ## Phase 4 - Output texture data; create index
+
+        outname = '%s%0*d' % (args.prefix, digits, i)
+        texname = outname + '.' + args.format
+        idxname = outname + '.' + 'idx'
+
+        print '\t', texname
+
+        texture.save(texname)
+
+        if args.encrypt:
+            encrypt_data(texname, args.encrypt, args.key, args.key_hash, args.key_file)
+            encrypt_data(idxname, args.encrypt, args.key, args.key_hash, args.key_file)
+
+################################################################################
+
+if __name__ == '__main__':
+    main()
+
+################################################################################
+## EOF
+################################################################################
+
